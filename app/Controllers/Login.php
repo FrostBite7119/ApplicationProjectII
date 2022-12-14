2a42eef24bb62d7cf6e33ba65936b51a343ee269
Handy Setiawan <handy7119@gmail.com>
2022-12-14 11:20:39 +0700

    Memperbaiki login

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -23,16 +23,13 @@ class Login extends BaseController
         if($dataUser){
             $password = $this->request->getPost('password');
             if(password_verify($password, $dataUser['password'])){
-                session()->set([
-                    'email' => $dataUser['email'],
-                    'nama' => $dataUser['nama'],
-                    'role' => $dataUser['role']
-                ]);
-
                 if($dataUser['role'] == "user"){
+                    session()->set([
+                        'email' => $dataUser['email'],
+                        'nama' => $dataUser['nama'],
+                        'role' => $dataUser['role']
+                    ]);
                     return redirect()->to(base_url('user'));
-                }else{
-                    return redirect()->to(base_url());
                 }
             }
         }
@@ -50,13 +47,14 @@ class Login extends BaseController
         if($dataUser){
             $password = $this->request->getPost('password');
             if(password_verify($password, $dataUser['password'])){
-                session()->set([
-                    'email' => $dataUser['email'],
-                    'nama' => $dataUser['nama'],
-                    'role' => $dataUser['role']
-                ]);
+                
 
                 if($dataUser['role'] == "admin"){
+                    session()->set([
+                        'email' => $dataUser['email'],
+                        'nama' => $dataUser['nama'],
+                        'role' => $dataUser['role']
+                    ]);
                     return redirect()->to(base_url('admin'));
                 }else{
                     return redirect()->to(base_url());
